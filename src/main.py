b493f6225463a7b37294954c550b2fa8d1dec9e0
Noah Green <noah@noahgreen.com>
2015-12-27 11:53:47 -0500

    Made file paths relative.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 import re
-
+import os.path as osp
+import os
+par = os.pardir
+cur = os.curdir
+pat = osp.dirname(__file__)
 class Dic(dict):
 	def __missing__(self, key):
 		return 0
@@ -53,14 +57,19 @@ def getWord(sol, wordArr):
 				if w.find(l) != -1:
 					tup = tuple(x for x, i in enumerate(w) if i == l)
 					lDict[l][tup] += 1
+			#print(l, lDict[l])
 		sortData = sorted({l : len(v) for l,v in lDict.items()}.items(), key=lambda x: x[1], reverse=True)
+		#print(sortData)
 		final = ""
 		maxValue = sortData[0][1]
 		finDict = {}
 		for i, j in sortData:
 			if j == maxValue:
+				#print(i, lDict[i])
 				finDict[i] = sum(k for l, k in lDict[i].items())
+		#print(finDict)
 		final = ((sorted({l : v for l,v in finDict.items()}.items(), key=lambda x: x[1], reverse=True)))[0][0]
+		#print(final)
 		letters = letters.replace(final, "")
 		used += final
 		rWord = re.compile("^" + word.replace(" ", ('[' + letters + ']')) + "$")
@@ -77,16 +86,15 @@ def getWord(sol, wordArr):
 			fakeLet += final
 
 	print(sol, (len(used) - len(realLet)), (len(realLet) / len(used) * 100))
-	#print("\nSolution: {}\nWord Found: {}\nLetters Used: {}\nLetters in Word: {}\nLetters not in Word: {}\nLetters not Used: {}\nWrong Guesses: {}\nPrecent Correct Guesses: {:.2f}%".format(sol, word, used, realLet, fakeLet, letters, (len(used) - len(realLet)), (len(realLet) / len(used) * 100)))
 	return (len(realLet) / len(used) * 100)
 
 print("Hangman Solver Test: ")
-tFile = open("\\Users\\Noah\\Documents\\Python Scripts\\Text Files\\wordsEn.txt", "r")
-arr = set([i.strip("\n") for i in tFile.readlines()])
-tFile.close()
-nFile = open("\\Users\\Noah\\Documents\\Python Scripts\\Text Files\\testWords.txt", "r")
-wordList = set([i.strip("\n") for i in nFile.readlines()])
-nFile.close()
+with  open(osp.join(pat, par, "resources", "wordsEn.txt")) as tFile:
+	arr = set([i.strip("\n") for i in tFile.readlines()])
+#tFile = open("\\Users\\Noah\\Documents\\Python Scripts\\Text Files\\wordsEn.txt", "r")
+#nFile = open("\\Users\\Noah\\Documents\\Python Scripts\\Text Files\\testWords.txt", "r")
+with  open(osp.join(pat, par, "resources", "tesTWords.txt")) as nFile:
+	wordList = set([i.strip("\n") for i in nFile.readlines()])
 sol = "reline"
 word = " " * len(sol)
 wordArr = [i for i in arr if len(i) == len(word)]
@@ -96,3 +104,7 @@ for w in wordList:
 	avg += getWord(w, wordArr)
 
 print("Final:", (avg / len(wordList)))
+#print()
+#simpAlg(sol, wordArr)
+#getWord(sol, word, wordArr)
+#print(sol, word, letters, used, realLet, (len(realLet) / len(used) * 100), (len(used) - len(realLet)))
